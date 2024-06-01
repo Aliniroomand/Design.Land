b9903d40b79d315da098695512f9466a02c3d9f6
Aliniroomand <aliniroomand32@gmail.com>
2024-06-01 10:40:06 +0330

    Home:added all styles of home but framer-motion styles doesn't add correctly

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -10,16 +10,22 @@ import teachingLogo from "../assets/images/Root/teach_transparent.png"
 import { Link } from 'react-router-dom'
 import PagesTransition from './helper/PageTransition'
 //helpers
-import { useMotionValueEvent, useScroll , motion} from 'framer-motion'
+import {motion , useScroll, useSpring } from "framer-motion" 
+import { useRef } from 'react'
 
 
 const Home = () => {
-        const useScrollBasics=()=>{
-                const { scrollYProgress }= useScroll();
-                useMotionValueEvent(scrollYProgress,"change",(latest)=>{
-                        console.log(latest);
-                })
-        }
+        
+        // for parallax animated 
+        const ref=useRef(null)
+        const {scrollYProgress}=useScroll({target:ref})
+        const y = useSpring(scrollYProgress, {
+                stiffness: 100,  
+                damping: 30,   
+                restDelta: 0.001, 
+        });
+        
+        //________________ for parallax animated ________
     return (
         <PagesTransition >
         <>
@@ -33,42 +39,44 @@ const Home = () => {
         {/*Title part*/}
                 <section className='fixed z-10 w-full h-[20svh] top-0 '
 >
-                    <p className='fixed md:text-5xl lg:text-2xl text-3xl text-center text-slate-900  w-full font-semibold font-[nastaliq] py-6 leading-[4rem]' style={{ textShadow: '0px 0px 5px #fff' }}>به دنیای رنگی دیزاین لند خوش اومدی <br/>برای دیدن قسمتهای مختلف هنر ما  اسکرول کن</p>
+                    <p className='fixed md:text-5xl lg:text-2xl text-3xl text-center bg-[#ffffff3c] backdrop-blur-[1px] text-slate-900  w-full font-semibold font-[nastaliq] py-6 leading-[4rem]' style={{ textShadow: '0px 0px 5px #fff' }}>به دنیای رنگی دیزاین لند خوش اومدی <br/>برای دیدن قسمتهای مختلف هنر ما  اسکرول کن</p>
                 </section>
         {/*____Title part_____*/}
 {/* selecting Part */}
-        <section className='relative w-svw h-[80svh] top-[20svh] overflow-y-scroll'>
+        <section className='relative w-svw h-[80svh] top-[20svh] overflow-y-scroll overflow-x-hidden'>
         {/* Balloon part */}
-                <Link to="/balloon" className='absolute z-30 w-full h-svh top-0 wobble '>
-                        <img className="absolute  w-[70%]  h-2/3 left-[15%] images_shadow top-0" src={balloon} alt="balloon" />
-                        <p  className='absolute top-1/2 text-yellow-400  md:text-5xl lg:text-2xl lg:w-4/6 text-xl font-medium text-center w-2/4 left-1/4' style={{ textShadow: '0px 0px 5px #000 , 0 0 10px #000,0 0 7px #000,0 0 13px #000' }}>دیزاین با بادکنک<br/>و بادکنک آرایی</p>
+        <motion.div ref={ref} className='absolute z-30 w-full h-[80svh] top-0 wobble ' style={{ y: y.interpolate({ map: [0, 1], output: [0, -100] }) }}>
+                <Link to="balloon" className='absolute z-30 w-full h-[80svh] top-0 wobble '>
+                        <img className="absolute  w-full  h-full left-0 images_shadow top-0" src={balloon} alt="balloon" />
+                        <p  className='absolute top-3/4 text-yellow-100 rounded-3xl bg-[#ffffff33] backdrop-blur-[1px] md:text-5xl lg:text-2xl lg:w-4/6 text-2xl font-medium text-center py-5 w-2/4 left-1/4' style={{ textShadow: '0px 0px 5px #000 , 0 0 10px #000,0 0 7px #000,0 0 13px #000' }}>دیزاین با بادکنک<br/>و بادکنک آرایی</p>
                 </Link>
+        </motion.div>
         {/*____Balloon part_____*/}
 
         {/*Flowerpart*/}
-                <Link to="/flower" className='absolute z-30 w-svw h-svh top-[100svh] right-0 wobble'>
-                        <img className="absolute w-[70%] h-2/3 left-[15%] images_shadow top-0" src={flower_design} alt="flower_design" />
-                        <p  className='absolute top-1/2 text-yellow-400  md:text-5xl lg:text-2xl lg:w-4/6 text-xl font-medium text-center w-2/4 left-1/4' style={{ textShadow: '0px 0px 5px #000 , 0 0 10px #000,0 0 7px #000,0 0 13px #000' }}>دیزاین با گل <br/>و گل آرایی</p>
+                <Link to="flower" className='absolute z-30 w-svw h-[80svh] top-[80svh] right-0 wobble'>
+                        <img className="absolute w-full h-full left-0 images_shadow top-0" src={flower_design} alt="flower_design" />
+                        <p  className='absolute top-3/4 text-yellow-100 rounded-3xl bg-[#ffffff33] backdrop-blur-[1px] md:text-5xl lg:text-2xl lg:w-4/6 text-2xl font-medium text-center py-5 w-2/4 left-1/4' style={{ textShadow: '0px 0px 5px #000 , 0 0 10px #000,0 0 7px #000,0 0 13px #000' }}>دیزاین با گل <br/>و گل آرایی</p>
                 </Link>
         {/*____Flower part_____*/}
 
         {/*Fruit part*/}
-                <Link to="/fruit" className='absolute z-30 w-full h-svh top-[200svh] wobble'>
-                        <img className="absolute wobble w-[70%] h-2/3 left-[15%] images_shadow top-0" src={fruit_design} alt="fruit_design" />
-                        <p  className='absolute bottom-1/5 left-2 text-yellow-400 drop-shadow-8xl md:text-5xl lg:text-2xl lg:w-4/6 text-xl text_shadow font-medium text-center '>دیزاین با میوه <br/> و میوه آرایی</p>
+                <Link to="fruit" className='absolute z-30 w-full h-[80svh] top-[160svh] wobble'>
+                        <img className="absolute wobble w-full h-full left-0 images_shadow top-0" src={fruit_design} alt="fruit_design" />
+                        <p  className='absolute top-3/4 text-yellow-100 rounded-3xl bg-[#ffffff33] backdrop-blur-[1px] md:text-5xl lg:text-2xl lg:w-4/6 text-2xl font-medium text-center py-5 w-2/4 left-1/4' style={{ textShadow: '0px 0px 5px #000 , 0 0 10px #000,0 0 7px #000,0 0 13px #000' }}>دیزاین با میوه <br/> و میوه آرایی</p>
                 </Link>
         {/*____Fruit part______*/}
 
         {/*education part*/}
-                <Link to="/learning" className='absolute z-30 w-full h-svh top-[300svh] wobble '>
-                        <img className="absolute wobble w-[70%] h-2/3 left-[15%] images_shadow top-0" src={teachingLogo} alt="teachingLogo" />
-                        <p  className='absolute bottom-0 text-yellow-400 drop-shadow-8xl md:text-5xl lg:text-2xl lg:w-4/6 text-xl text_shadow font-medium text-center '>آموزش</p>
+                <Link to="/learning" className='absolute z-30 w-full h-[80svh] top-[240svh] wobble '>
+                        <img className="absolute wobble w-full h-full left-0 images_shadow top-0" src={teachingLogo} alt="teachingLogo" />
+                        <p  className='absolute top-3/4 text-yellow-100 rounded-3xl bg-[#ffffff33] backdrop-blur-[1px] md:text-5xl lg:text-2xl lg:w-4/6 text-3xl font-medium text-center py-5 w-2/4 left-1/4' style={{ textShadow: '0px 0px 5px #000 , 0 0 10px #000,0 0 7px #000,0 0 13px #000' }}>آموزش</p>
                 </Link>
         {/*____education  part_____*/}
         {/*Contact part*/}
-                <Link to='commentsAndContact' className='absolute z-30 w-full h-svh top-[400svh] wobble'>
-                        <img className="absolute wobble w-[70%] h-2/3 left-[15%] images_shadow top-0" src={contatct_us} alt="contatct_us" />
-                        <p  className='absolute bottom-5 text-yellow-400 drop-shadow-8xl md:text-5xl lg:text-2xl lg:w-4/6 text-xl text_shadow font-medium text-center '>تماس با ما<br/>و نظرات</p>
+                <Link to='commentsAndContact' className='absolute z-30 w-full h-[80svh] top-[320svh] wobble'>
+                        <img className="absolute wobble w-full h-full left-0 images_shadow top-0" src={contatct_us} alt="contatct_us" />
+                        <p  className='absolute top-3/4 text-yellow-100 rounded-3xl bg-[#ffffff33] backdrop-blur-[1px] md:text-5xl lg:text-2xl lg:w-4/6 text-2xl font-medium text-center py-5 w-2/4 left-1/4' style={{ textShadow: '0px 0px 5px #000 , 0 0 10px #000,0 0 7px #000,0 0 13px #000' }}>تماس با ما<br/>و نظرات</p>
                 </Link>
                 
     {/*____Contact part_______*/}
---
+++ src/helper/ParallaxScroll.tsx
@@ -0,0 +1,10 @@
+import {   motion,
+    useScroll,
+    useSpring,
+    useTransform,
+    MotionValue} from 'framer-motion'
+
+
+
+export default function useParallax(value: MotionValue<number>, distance: number) {
+    return useTransform(value, [0, 1], [-distance, distance]);}
